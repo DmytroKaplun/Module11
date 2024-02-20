417b09c106d02813d424ae160c2da0e3fb34f4a8
DmytroKaplun <newtonknight1990@gmail.com>
2024-02-20 11:56:10 +0200

    second

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,67 @@
 package org.example;
 
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<String> nameList = List.of("Alex", "Chris", "Ivan", "Peter", "Bob", "Oleg", "Dmytro");
+        //1 task
+//        String oddName = nameToString(nameList);
+//        System.out.println(oddName);
+        //2 task
+//        List<String> sortedNameList = sortName(nameList);
+//        System.out.println(sortedNameList);
+        //3 task
+//        String[] stringArray = {"1, 2, 0", "4, 5"};
+//        String result = Arrays.stream(stringArray)
+//                .flatMap(str -> Arrays.stream(str.split(", ")))
+//                .map(Integer::parseInt)
+//                .sorted()
+//                .map(String::valueOf)
+//                .collect(Collectors.joining(", "));
+//        System.out.println(result);
+        //4 task
+//        long a = 25214903917L;
+//        long c = 11L;
+//        long m = 2^48L;
+//        Stream<Long> stream = generateRandom(a, c, m);
+//        stream.limit(10).forEach(System.out::println);
+        //5 task
+//        Stream<String> stream1 = Stream.of("A", "B", "C", "D");
+//        Stream<String> stream2 = Stream.of("1", "2", "3", "4", "5", "6");
+//        Stream<String> resStream = zip(stream2, stream1);
+//        resStream.forEach(System.out::println);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
+
+    public static String nameToString(List<String> nameList) {
+        return nameList.stream()
+                .filter(name -> (nameList.indexOf(name)+1)%2 != 0)
+                .map(name -> (nameList.indexOf(name)+1) + ". " + name)
+                .collect(Collectors.joining(", "));
+    }
+
+    public static List<String> sortName(List<String> nameList) {
+        return nameList.stream()
+                .map(String::toUpperCase)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+    }
+
+    public static Stream<Long> generateRandom(long a, long c, long m) {
+        long seed = 0;
+        return Stream.iterate(seed, i -> ((i + 1) * a + c) % m);
+    }
+
+    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
+        Iterator<T> secStr = second.iterator();
+        return first.flatMap(str -> secStr.hasNext() ? Stream.of(str, secStr.next()) : Stream.empty());
+    }
+
 }
